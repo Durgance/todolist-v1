8402cd0a72bd59986084c7c0e6e3d481bbed8c6f
Durgance Gaur <gdurgance@gmail.com>
2020-08-14 18:22:20 +0530

    add ejs template

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine","ejs");
+app.set("view engine","ejs");
 
 app.get("/", function(req, res){
   var today =new Date();
   today = today.getDay();
+  var day="";
   if (today==6 || today==0){
-    res.send("its the weekend");
-
+    day+="weekend";
+    
   }else{
-    res.send("its a workday");
+    day+="weekday";
   }
+  res.render("list",{kindOfDay:day});
   //res.send("Hello");
 });
- 
-app.listen( process.env.PORT ||3000, function(){
+
+app.listen( process.env.PORT || 3000 , function(){
   console.log("Server started on port : 3000");
 });
