d973dc39a5df6c6377ef8b3f80dc2e1f7f17f0ad
Durgance Gaur <gdurgance@gmail.com>
2020-08-14 19:56:23 +0530

    Complete version-1

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = [];
+app.set("view engine", "ejs");
 
-app.set("view engine","ejs");
+app.use(express.static(__dirname + "/Public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-  var today =new Date();
-  today = today.getDay();
-  var day="";
-  if (today==6 || today==0){
-    day+="weekend";
-    
-  }else{
-    day+="weekday";
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  if (item!==""){
+	items.push(item);
   }
-  res.render("list",{kindOfDay:day});
+  
+  
+  res.redirect("/");
+});
+
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let day = "";
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newListItems: items });
   //res.send("Hello");
 });
 
-app.listen( process.env.PORT || 3000 , function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port : 3000");
 });
